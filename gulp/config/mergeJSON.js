b084e7bc7fca6eee65f2d64decfff5426e4865ec
dgarnitskiy <danya.garnitskiy@gmail.com>
2025-04-16 19:37:55 +0300

    recursive func in mergeJSON

--- gulp/config/mergeJSON.js
+++ gulp/config/mergeJSON.js
@@ -1,15 +1,25 @@
 import fs from 'fs'
+import path from 'path'
 
 export default function mergeJSON(dir) {
-	const files = fs.readdirSync(dir).filter(file => file.endsWith('.json'))
-
 	let mergedData = {}
 
-	files.forEach(file => {
-		const filePath = `${dir}/${file}`
-		const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'))
-		mergedData = { ...mergedData, ...jsonData }
-	})
+	function readDir(currentDir) {
+		const entries = fs.readdirSync(currentDir, { withFileTypes: true })
+
+		entries.forEach(entry => {
+			const fullPath = path.join(currentDir, entry.name)
+
+			if (entry.isDirectory()) {
+				readDir(fullPath)
+			} else if (entry.isFile() && fullPath.endsWith('.json')) {
+				const jsonData = JSON.parse(fs.readFileSync(fullPath, 'utf8'))
+				mergedData = { ...mergedData, ...jsonData }
+			}
+		})
+	}
+
+	readDir(dir)
 
 	return mergedData
 }
